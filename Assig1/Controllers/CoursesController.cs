04c0fab42d6737108b9e318de0fcecf3846e765e
Rua <1221727@student.birzeit.edu>
2026-07-20 22:16:28 +0300

    Initial ASP.NET Core Web API assignment

---
+++ Assig1/Controllers/CoursesController.cs
@@ -0,0 +1,76 @@
+﻿using Assig1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assig1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        private static List<Course> courses = new List<Course>
+        {
+            new Course
+            {
+                Id = 1,
+                Name = "Database",
+                Hours = 3
+            },
+            new Course
+            {
+                Id = 2,
+                Name = "Operating Systems",
+                Hours = 3
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Course>> GetAllCourses()
+        {
+            return Ok(courses);
+        }
+        [HttpPost]
+        public ActionResult AddCourse(Course course)
+        {
+            bool idExists = courses.Any(c => c.Id == course.Id);
+
+            if (idExists)
+            {
+                return BadRequest("A course with the same Id already exists.");
+            }
+
+            courses.Add(course);
+
+            return Ok(course);
+        }
+        [HttpPut("{id}")]
+        public ActionResult UpdateCourse(int id, Course updatedCourse)
+        {
+            Course? existingCourse =
+                courses.FirstOrDefault(c => c.Id == id);
+
+            if (existingCourse == null)
+            {
+                return NotFound();
+            }
+
+            existingCourse.Name = updatedCourse.Name;
+            existingCourse.Hours = updatedCourse.Hours;
+
+            return Ok(existingCourse);
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCourse(int id)
+        {
+            Course? course = courses.FirstOrDefault(c => c.Id == id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            courses.Remove(course);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Assig1/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assig1.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(1, 4)]
+        public int Hours { get; set; }
+    }
+}
